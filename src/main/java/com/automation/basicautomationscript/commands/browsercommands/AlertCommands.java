1e0c1c6f1f096b5d2ca70d248d700177542625c8
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-05-20 19:47:22 +0530

    alertWithOkAndCancel added

--- src/main/java/com/automation/basicautomationscript/commands/browsercommands/AlertCommands.java
+++ src/main/java/com/automation/basicautomationscript/commands/browsercommands/AlertCommands.java
@@ -7,7 +7,8 @@ import org.openqa.selenium.WebDriver;
 
 public class AlertCommands {
     public static void main(String[] args) throws InterruptedException {
-        alertWithOK();
+//        alertWithOK();
+        alertWithOkAndCancel();
     }
 
     public static void alertWithOK() throws InterruptedException {
@@ -23,5 +24,21 @@ public class AlertCommands {
         System.out.println(alert.getText());
         alert.accept();
         Thread.sleep(3000);
+    }
+
+    public static void alertWithOkAndCancel() throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+        driver.get("https://demo.automationtesting.in/Alerts.html");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[text()='Alert with OK & Cancel ']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("[class='btn btn-primary']")).click();
+        Thread.sleep(3000);
+        Alert alert = driver.switchTo().alert();
+        //alert.accept();
+        alert.dismiss();
+        System.out.println(driver.findElement(By.id("demo")).getText());
+
     }
 }
