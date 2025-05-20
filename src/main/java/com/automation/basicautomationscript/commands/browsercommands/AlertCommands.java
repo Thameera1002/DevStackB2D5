978e4e5d4792e50aaf78fb0f03bffa48364f4ad1
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-05-20 19:39:55 +0530

    alertWithOK added

---
+++ src/main/java/com/automation/basicautomationscript/commands/browsercommands/AlertCommands.java
@@ -0,0 +1,27 @@
+package com.automation.basicautomationscript.commands.browsercommands;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AlertCommands {
+    public static void main(String[] args) throws InterruptedException {
+        alertWithOK();
+    }
+
+    public static void alertWithOK() throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+        driver.get("https://demo.automationtesting.in/Alerts.html");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[text()='Alert with OK ']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("[class='btn btn-danger']")).click();
+        Thread.sleep(3000);
+        Alert alert = driver.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.accept();
+        Thread.sleep(3000);
+    }
+}
