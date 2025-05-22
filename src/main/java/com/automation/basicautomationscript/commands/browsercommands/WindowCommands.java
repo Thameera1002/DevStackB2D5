4d24b68e56d0c98407530aa79eff2b22349b6b83
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-05-22 19:21:17 +0530

    WindowCommands.java updated

--- src/main/java/com/automation/basicautomationscript/commands/browsercommands/WindowCommands.java
+++ src/main/java/com/automation/basicautomationscript/commands/browsercommands/WindowCommands.java
@@ -8,7 +8,8 @@ import java.util.Set;
 
 public class WindowCommands {
     public static void main(String[] args) throws InterruptedException {
-        newTabbedWindow();
+        //newTabbedWindow();
+        openNewSeperateWindow();
     }
 
     public static void newTabbedWindow() throws InterruptedException {
@@ -40,5 +41,37 @@ public class WindowCommands {
         driver.switchTo().window(parentWindow);
         Thread.sleep(3000);
         System.out.println(driver.getTitle());
+    }
+
+    public static void openNewSeperateWindow() throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+        driver.get("https://demo.automationtesting.in/Windows.html");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[text()='Open New Seperate Windows']")).click();
+        Thread.sleep(3000);
+
+        //Store the parent window
+        String parentWindow = driver.getWindowHandle();
+        System.out.println("Parent Window: " + parentWindow);
+
+        driver.findElement(By.xpath("//button[text()='click']")).click();
+        Thread.sleep(3000);
+        System.out.println(driver.getTitle());
+
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String window : allWindows) {
+            if (!window.equals(parentWindow)) {
+                driver.switchTo().window(window);
+                break;
+            }
+        }
+        Thread.sleep(3000);
+        System.out.println(driver.getTitle());
+        Thread.sleep(3000);
+        driver.switchTo().window(parentWindow);
+        Thread.sleep(3000);
+        System.out.println(driver.getTitle());
+
     }
 }
