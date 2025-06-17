4e4ce38b9978934e738285884be1b8750632a254
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-06-17 18:38:53 +0530

    Threadlocal webdriver example classes added. old testng file renamed as testngOld3 and new testng file added.

---
+++ src/test/java/testThreadLocal/TestB.java
@@ -0,0 +1,18 @@
+package testThreadLocal;
+
+import org.testng.annotations.Test;
+
+public class TestB extends BaseTest{
+    @Test
+    public void testB1() throws InterruptedException {
+        driver.get("https://github.com/");
+        System.out.println(driver.getTitle());
+        Thread.sleep(1500);
+    }
+    @Test
+    public void testB2() throws InterruptedException {
+        driver.get("http://stackoverflow.com/questions");
+        System.out.println(driver.getTitle());
+        Thread.sleep(1500);
+    }
+}
