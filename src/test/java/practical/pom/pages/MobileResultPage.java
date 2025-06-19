37318c2017fc412cdfc800b7b27feac053fa98be
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-06-19 19:07:55 +0530

    MobileResultPage.java added

---
+++ src/test/java/practical/pom/pages/MobileResultPage.java
@@ -0,0 +1,14 @@
+package practical.pom.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MobileResultPage {
+
+    @FindBy(xpath = "//span[text()='Apple']")
+    private WebElement appleCheckBox;
+
+    public void clickAppleCheckBox() {
+        appleCheckBox.click();
+    }
+}
