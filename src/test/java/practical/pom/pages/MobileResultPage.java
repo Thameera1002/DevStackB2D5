73ef09f0df554176fb02374b9cb40e00579e3fdb
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-06-19 19:22:52 +0530

    BasePage.java & BaseTest.java added. pages updated

--- src/test/java/practical/pom/pages/MobileResultPage.java
+++ src/test/java/practical/pom/pages/MobileResultPage.java
@@ -1,12 +1,17 @@
 package practical.pom.pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class MobileResultPage {
+public class MobileResultPage extends BasePage {
 
     @FindBy(xpath = "//span[text()='Apple']")
     private WebElement appleCheckBox;
+
+    public MobileResultPage(WebDriver driver) {
+        super(driver);
+    }
 
     public void clickAppleCheckBox() {
         appleCheckBox.click();
