7bb2b359fbfece0bbb6bbe2f35a3a656d241b5e7
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-06-24 19:52:46 +0530

    TestDependsOnGroup.java added to identify depends on groups.

---
+++ src/test/java/TestDependsOnGroup.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class TestDependsOnGroup {
+    @Test(groups = "login")
+    public void login1(){
+        System.out.println("login1");
+    }
+
+    @Test(groups = "login")
+    public void login2(){
+        System.out.println("login2");
+    }
+
+    @Test(dependsOnGroups = {"login"})
+    public void dashboard(){
+        System.out.println("dashboard");
+    }
+}
