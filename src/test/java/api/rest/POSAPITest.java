e5ac1566fb2bf02444fdb76afe5cd44297173a51
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-07-15 01:51:07 +0530

    POSAPITest.java updated.

--- src/test/java/api/rest/POSAPITest.java
+++ src/test/java/api/rest/POSAPITest.java
@@ -1,8 +1,10 @@
 package api.rest;
 
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class POSAPITest {
 
@@ -16,4 +18,23 @@ public class POSAPITest {
                 .statusCode(200)
                 .log().all();
     }
+
+    @Test
+    public void createNewCustomer(){
+        String newCustomer = "    {\n" +
+                "        \"name\": \"New Customer 1\",\n" +
+                "        \"address\": \"New Address 1\",\n" +
+                "        \"salary\": 68000\n" +
+                "    }";
+        given()
+                .contentType(ContentType.JSON)
+                .body(newCustomer)
+                .when()
+                .post("http://localhost:3000/api/customers")
+                .then()
+                .assertThat()
+                .body("name",equalTo("New Customer 1"))
+                .log().all();
+    }
+
 }
