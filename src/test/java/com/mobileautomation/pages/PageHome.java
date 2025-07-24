913857c4944b5e408c5a65778dee803871684154
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-07-24 19:04:12 +0530

    locators added to PageLogin.java & PageHome.java.

--- src/test/java/com/mobileautomation/pages/PageHome.java
+++ src/test/java/com/mobileautomation/pages/PageHome.java
@@ -1,9 +1,14 @@
 package com.mobileautomation.pages;
 
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PageHome extends BasePage{
     public PageHome(WebDriver driver) {
         super(driver);
     }
+
+    @AndroidFindBy(accessibility = "Login")
+    private WebElement nav_loginIcon;
 }
--- src/test/java/com/mobileautomation/pages/PageLogin.java
+++ src/test/java/com/mobileautomation/pages/PageLogin.java
@@ -1,9 +1,20 @@
 package com.mobileautomation.pages;
 
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PageLogin extends BasePage{
     public PageLogin(WebDriver driver) {
         super(driver);
     }
+
+    @AndroidFindBy(accessibility = "input-email")
+    private WebElement tf_userName;
+
+    @AndroidFindBy(accessibility = "input-password")
+    private WebElement tf_password;
+
+    @AndroidFindBy(accessibility = "button-LOGIN")
+    private WebElement btn_login;
 }
